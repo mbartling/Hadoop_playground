e702afc793a80c9d53d5e3f1a4c85b41e17257c2
Michael Bartling <michael.bartling15@gmail.com>
2014-01-13 08:48:08 -0600

    cleaned up input generation

--- C_MinMax/src/geninputs.c
+++ C_MinMax/src/geninputs.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define NUMLINES 1000
+#define NUM_ENTRY_LINE 10
 int main()
 {
     int number;
     int maxnumber;
     int i,j;
     srand(time(NULL));
-    maxnumber = 1<<30;
+
     FILE * fp;
     fp = fopen("file01", "w");
 
-    for(i = 0; i < 1000; i++)
+    for(i = 0; i < NUMLINES; i++)
     {
-        for(j = 1; j<10; j++)
+        for(j = 1; j<NUM_ENTRY_LINE; j++)
         {
 
-            number = rand() % maxnumber;
+            number = rand();
             fprintf(fp, "%d ", number);
         }
         fprintf(fp,"\n");
