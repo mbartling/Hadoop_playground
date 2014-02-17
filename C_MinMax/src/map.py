c0a7d74364db9ed8b0bb2fcb81400da5bc41c818
Michael Bartling <michael.bartling15@gmail.com>
2014-02-17 12:32:03 -0600

    Updated functional python tests

--- C_MinMax/src/map.py
+++ C_MinMax/src/map.py
@@ -1,9 +1,14 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 import sys
+from operator import itemgetter
 
 for line in sys.stdin:
     val = line.strip()
     val = val.split()
+    #try:
+    #    val = float(val)
+    #except ValueError:
+    #    continue
     maxVal = max(val)
     minVal = min(val)
     print "Max\t%s" % maxVal
--- C_MinMax/src/reduce.py
+++ C_MinMax/src/reduce.py
@@ -1,16 +1,23 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
+from operator import itemgetter
 import sys
 
-minVal = sys.float_info.max
-maxVal = sys.float_info.min
-
+#minVal = sys.float_info.max
+#maxVal = sys.float_info.min
+minVal = sys.maxint
+maxVal = -sys.maxint -1
 for line in sys.stdin:
-    (key, value) = line.strip().split("\t")
+    (key, value) = line.strip().split('\t')
+    try:
+        value = int(value)
+    except ValueError:
+        continue
+
     if key == "Max":
         maxVal = max(maxVal, value)
     elif key == "Min":
         minVal = min(minVal, value)
 
-print "Max\t%s" % maxVal
-print "Min\t%s" % minVal
+print 'Max\t%s' % maxVal
+print 'Min\t%s' % minVal
