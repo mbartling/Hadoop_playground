d352504d6e004190c0ef2eee71b54fd559c33227
Michael Bartling <michael.bartling15@gmail.com>
2014-03-27 14:27:05 -0500

    Updated the reduce script to run miniSAT

--- FrogSAT/reduceCNF.py
+++ FrogSAT/reduceCNF.py
@@ -5,6 +5,19 @@ import os
 literals = set()
 clauseList = []
 
+WINDOWSMODE = 1
+
+def runMiniSat(filename):
+    if WINDOWSMODE == 1:
+        minisat = "./minisat_static.exe " 
+        minisat = minisat + filename + " -no-luby -rinc=1.5 -phase-saving=0 -rnd-freq=0.02"
+        os.system(minisat)
+    else:
+        minisat = "./minisat_static " 
+        minisat = minisat + filename + " -no-luby -rinc=1.5 -phase-saving=0 -rnd-freq=0.02"
+        os.system(minisat)
+    
+
 #prevKey = ''
 line  = sys.stdin.readline()
 line = line.strip()
@@ -23,39 +36,40 @@ for line in sys.stdin:
     (key,clause) = line.split('\t')
 
     if key != prevKey:
-        numLits = len(literals)/2 # Should be even number
-        filenameTxt = prevKey.replace(' ', '-')
-        filename = '{0}.cnf'.format(filenameTxt)
-        
-        # Open the file to work on
-        f = open(filename, 'w')
+    	numLits = len(literals)/2 # Should be even number
+    	filenameTxt = prevKey.replace(' ', '-')
+    	filename = '{0}.cnf'.format(filenameTxt)
+    	
+    	# Open the file to work on
+    	f = open(filename, 'w')
 
-        # write out problem statement
-        # p cnf numliterals numClauses\n
-        #problem  = ('p cnf', numLits, len(clauseList), '\n')
-        problem = 'p cnf {0} {1}\n'.format(numLits, len(clauseList))
-        f.write(problem)
+    	# write out problem statement
+    	# p cnf numliterals numClauses\n
+    	#problem  = ('p cnf', numLits, len(clauseList), '\n')
+    	problem = 'p cnf {0} {1}\n'.format(numLits, len(clauseList))
+    	f.write(problem)
 
-        # Write the clauses
-        #f.writelines(clauseList)
-        for entry in clauseList:
-            f.write('{0} 0\n'.format(entry))
-        
-	f.close()
-        
-        # Run the command line minisat here
-        print 'Running Minisat Dummy'
-	os.system("cat " + filename+ ">" + '{0}.out'.format(filenameTxt))
-        literals.clear()
-        clauseList = []
-    
+    	# Write the clauses
+    	#f.writelines(clauseList)
+    	for entry in clauseList:
+    	    f.write('{0} 0\n'.format(entry))
+    	
+    	f.close()
+    	
+    	# Run the command line minisat here
+    	print 'Running Minisat Dummy'
+    	#os.system("cat " + filename+ ">" + '{0}.out'.format(filenameTxt))
+    	runMiniSat(filename)
+    	literals.clear()
+    	clauseList = []
+    	
     # Working on same key
     lits = clause.split()
     prevKey = key
     for literal in lits:
-        literal = int(literal)
-        literals.add(literal)
-        literals.add(-literal)
+    	literal = int(literal)
+    	literals.add(literal)
+    	literals.add(-literal)
     clauseList.append(clause)
 
 # Process the last key
@@ -77,11 +91,12 @@ f.write(problem)
 #f.writelines(clauseList)
 for entry in clauseList:
     f.write('{0} 0\n'.format(entry))
-        
+    
 f.close()
 
 # Run the command line minisat here
 print 'Running Minisat Dummy'
-os.system("cat " + filename+ ">" + '{0}.out'.format(filenameTxt))
+#os.system("cat " + filename+ ">" + '{0}.out'.format(filenameTxt))
+runMiniSat(filename)
 literals.clear()
 clauseList = []
