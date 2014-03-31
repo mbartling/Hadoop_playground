cb4494ae9aa0eb4e951ce8bb3421bef22eee9f64
Michael Bartling <michael.bartling15@gmail.com>
2014-03-31 11:52:33 -0500

    Added modified MiniSAT to the git repository

---
+++ FrogSAT/minisat/core/depend.mk
@@ -0,0 +1,29 @@
+/cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.o /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.or /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.od /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.op: \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.cc \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/System.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/IntTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/ParseUtils.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/Options.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Vec.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/XAlloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Dimacs.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/SolverTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alg.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Map.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Heap.h
+/cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.o /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.or /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.od /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.op: \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.cc \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Sort.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Vec.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/IntTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/XAlloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Heap.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alg.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/Options.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/ParseUtils.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/SolverTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Map.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alloc.h
---
+++ FrogSAT/minisat/simp/depend.mk
@@ -0,0 +1,63 @@
+/cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/Main.o /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/Main.or /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/Main.od /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/Main.op: \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/Main.cc \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/System.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/IntTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/ParseUtils.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/Options.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Vec.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/XAlloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Dimacs.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/SolverTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alg.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Map.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/SimpSolver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Queue.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Heap.h
+/cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/SimpSolver.o /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/SimpSolver.or /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/SimpSolver.od /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/SimpSolver.op: \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/SimpSolver.cc \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Sort.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Vec.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/IntTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/XAlloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/simp/SimpSolver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Queue.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Heap.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alg.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/Options.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/ParseUtils.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/SolverTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Map.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/System.h
+/cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.o /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.or /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.od /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.op: \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Main.cc \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/System.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/IntTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/ParseUtils.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/Options.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Vec.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/XAlloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Dimacs.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/SolverTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alg.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Map.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Heap.h
+/cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.o /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.or /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.od /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.op: \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.cc \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Sort.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Vec.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/IntTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/XAlloc.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/Solver.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Heap.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alg.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/Options.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/utils/ParseUtils.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/core/SolverTypes.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Map.h \
+ /cygdrive/c/Users/Mike/git/Hadoop_playground/FrogSAT/minisat/mtl/Alloc.h
