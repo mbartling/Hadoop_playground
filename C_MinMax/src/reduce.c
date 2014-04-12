f8227906aa87b9f26c54d54c4c9ee9315da85369
Michael Bartling <michael.bartling15@gmail.com>
2014-04-12 15:07:46 -0500

    Update reduce.c

--- C_MinMax/src/reduce.c
+++ C_MinMax/src/reduce.c
@@ -26,8 +26,8 @@ void myReduceLines(void)
 	int32_t key;
 	int32_t prev_key;
 
-	double min_val;
-	double max_val;
+	//double min_val;
+	//double max_val;
 	double val;
 	double local_max_val;
 	double local_min_val;
@@ -36,6 +36,37 @@ void myReduceLines(void)
 	local_min_val = DBL_MAX;
 	local_max_val = DBL_MIN;
 	prev_key = 0;
+	
+	while (scanf("%d\t%lf\n",&key, &val) > 0)
+	{
+
+		switch(key)
+		{
+			case key_max:
+			{
+				local_max_val 	= (val > local_max_val) ? val: local_max_val;
+				break
+			}
+			case key_min:
+			 {
+			 	local_min_val 	= (val < local_min_val) ? val: local_min_val;
+			 	break;
+			 }
+			default: break; //silently ignore
+		}
+
+	}// End While
+
+	//We can write the max and min out like this, however it is safer if we use our previous convention. 
+	// This will let us "chain" reduce methods.
+	//printf("Max\t%lf\n", local_max_val);
+	//printf("Min\t%lf\n", local_min_val);
+	
+	printf("%d\t%lf\n", key_max, local_max_val);
+	printf("%d\t%lf\n", key_min, local_min_val);
+	
+	// If we want to ignore the key all together uncomment this section
+	/*
 	while (scanf("%d\t%lf\n",&key, &val) > 0)
 	{
 
@@ -43,25 +74,26 @@ void myReduceLines(void)
 		if( prev_key && prev_key != key)
 		{
 
-			/* Update to new key set */
+			// Update to new key set 
 			prev_key = key;
 			local_min_val 	= (val < local_min_val) ? val: local_min_val;
 			local_max_val 	= (val > local_max_val) ? val: local_max_val;
 
 		}
-		else /* We have the first key set or are processing the same key set*/
+		else // We have the first key set or are processing the same key set
 		{
 
-			/* Process each line separately */
+			// Process each line separately
 			prev_key 		= key;
 			local_min_val 	= (val < local_min_val) ? val: local_min_val;
 			local_max_val 	= (val > local_max_val) ? val: local_max_val;
-		} /* End Else*/
+		} // End Else
 
-	} /* End While */
-
+	} // End While 
+	
 	printf("Max\t%lf\n", local_max_val);
 	printf("Min\t%lf\n", local_min_val);
+	*/
 
 
 
