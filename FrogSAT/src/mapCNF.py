0ec34c3af0ef0c98b92b76b754b616c6f68218b3
Michael Bartling <michael.bartling15@gmail.com>
2014-04-21 16:05:08 -0500

    major cleanup

---
+++ FrogSAT/src/mapCNF.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import sys
+sys.path.append('.')
+f = open('mapKeyList.txt', 'r')
+
+keyList = []
+# make a list of all of the relevant keys
+for line in f:
+	line.strip()
+	keyList.append(line)
+
+# Assume that there are no comment blocks or problem statements in this file
+for line in sys.stdin:
+	line = line.strip()
+	lineS = line.split()
+    # For each line we receive, ignore comments and problem statements
+	if lineS[0] != 'c' and lineS[0] != 'p':
+		for key in keyList:
+			(key1, key2) = key.split()
+			#print key1, key2
+            # if either A or ~A or B or ~B in the clause then print out
+            # key1 key2 \t clause, where key1 < key2
+			if (key1 in lineS) or (key2 in lineS):
+				keyS= ''.join(key)
+				keyS = keyS.strip('\n')
+				keyS = '{0}\t'.format(keyS)
+				print keyS + line
+			if ('-{0}'.format(key1) in line) or ('-{0}'.format(key2) in line):
+				keyS = ''.join(key)
+				keyS = keyS.strip('\n')
+				keyS = '{0}\t'.format(keyS)
+				print keyS + line
---
+++ FrogSAT/src/reduceCNF.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python
+import sys
+import os
+sys.path.append('.')
+literals = set()
+clauseList = []
+
+WINDOWSMODE = 0
+verbosity = 0
+MSVERBOSITY = " -verb={0}".format(verbosity)
+def runMiniSat(filename, filenameout):
+    if WINDOWSMODE == 1:
+        minisat = "./minisat_static.exe " 
+        minisat = minisat + filename + " " + filenameout + MSVERBOSITY + " -no-luby -rinc=1.5 -phase-saving=0 -rnd-freq=0.02"
+        os.system(minisat)
+    else:
+        minisat = "./minisat_static " 
+        minisat = minisat + filename + " " + filenameout + MSVERBOSITY +  " -no-luby -rinc=1.5 -phase-saving=0 -rnd-freq=0.02"
+        os.system(minisat)
+    
+
+#prevKey = ''
+line  = sys.stdin.readline()
+line = line.strip()
+(key,clause) = line.split('\t')
+lits = clause.split()
+prevKey = key
+
+for literal in lits:
+    literal = int(literal)
+    literals.add(literal)
+    literals.add(-literal)
+clauseList.append(clause)
+
+for line in sys.stdin:
+    line = line.strip()
+    (key,clause) = line.split('\t')
+
+    if key != prevKey:
+        #numLits = len(literals)/2 # Should be even number
+        # MiniSAT determines the number of literals and compares it to our problem statement. Note miniSat is lazy and looks for the max(abs) of the literals.
+        numLits = max(literals)
+        filenameTxt = prevKey.replace(' ', '-')
+        filename = '{0}.cnf'.format(filenameTxt)
+        filenameout = '{0}-MiniSat.out'.format(filenameTxt)        
+        # Open the file to work on
+        f = open(filename, 'w')
+
+        # write out problem statement
+        # p cnf numliterals numClauses\n
+        #problem  = ('p cnf', numLits, len(clauseList), '\n')
+        problem = 'p cnf {0} {1}\n'.format(numLits, len(clauseList))
+        # MiniSAT doesnt need this, see www.msoos.org/minisat-faq/
+        f.write(problem)
+
+        # Write the clauses
+        #f.writelines(clauseList)
+        for entry in clauseList:
+            f.write('{0}\n'.format(entry))
+        
+        f.close()
+        
+        # Run the command line minisat here
+        print 'Running Minisat Dummy'
+        #os.system("cat " + filename+ ">" + '{0}.out'.format(filenameTxt))
+        runMiniSat(filename, filenameout)
+        literals.clear()
+        clauseList = []
+        
+    # Working on same key
+    lits = clause.split()
+    prevKey = key
+    for literal in lits:
+        literal = int(literal)
+        literals.add(literal)
+        literals.add(-literal)
+    clauseList.append(clause)
+
+# Process the last key
+
+#numLits = len(literals)/2 # Should be even number
+numLits = max(literals)
+filenameTxt = key.replace(' ', '-')
+filename = '{0}.cnf'.format(filenameTxt)
+filenameout = '{0}-MiniSat.out'.format(filenameTxt)        
+# Open the file to work on
+f = open(filename, "w")
+
+# write out problem statement
+# p cnf numliterals numClauses\n
+#problem  = ('p cnf', numLits, len(clauseList), '\n')
+problem = 'p cnf {0} {1}\n'.format(numLits, len(clauseList))
+f.write(problem)
+
+# Write the clauses
+#f.writelines(clauseList)
+for entry in clauseList:
+    f.write('{0}\n'.format(entry))
+    
+f.close()
+
+# Run the command line minisat here
+print 'Running Minisat Dummy'
+#os.system("cat " + filename+ ">" + '{0}.out'.format(filenameTxt))
+runMiniSat(filename, filenameout)
+literals.clear()
+clauseList = []
